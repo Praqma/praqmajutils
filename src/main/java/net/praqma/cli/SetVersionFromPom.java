e0f1a772c67e9a2c13ae7018ae9f720d092a0d8a
wolfgang <chw@praqma.net>
2011-03-01 14:00:53 +0100

    Deleted some imports

--- src/main/java/net/praqma/cli/SetVersionFromPom.java
+++ src/main/java/net/praqma/cli/SetVersionFromPom.java
@@ -2,8 +2,6 @@ package net.praqma.cli;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import net.praqma.util.io.BuildNumberStamper;
 import net.praqma.util.io.Pom;
