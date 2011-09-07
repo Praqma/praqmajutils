db827c343bcb0fa44da0ae011a50e85c8f0d9e8f
wolfgarnet <wolfgarnet@gmail.com>
2011-09-07 11:48:21 +0200

    Updated options

--- src/main/java/net/praqma/util/execute/CommandLine.java
+++ src/main/java/net/praqma/util/execute/CommandLine.java
@@ -1,6 +1,5 @@
 package net.praqma.util.execute;
 
-
 import java.io.File;
 import java.io.IOException;
 
@@ -9,110 +8,98 @@ import net.praqma.util.debug.PraqmaLogger.Logger;
 
 /**
  * CLI class
+ * 
  * @author wolfgang
- *
+ * 
  */
-public class CommandLine implements CommandLineInterface
-{
+public class CommandLine implements CommandLineInterface {
 	protected Logger logger = PraqmaLogger.getLogger();
 	protected static final String linesep = System.getProperty( "line.separator" );
-	
+
 	private static CommandLine instance = new CommandLine();
-	
+
 	private String os = null;
 	private OperatingSystem thisos = OperatingSystem.WINDOWS;
 	private String[] cmd = null;
 	private int last = 0;
-	
 
-	
-	public void setLogger( Logger logger )
-	{
+	public void setLogger( Logger logger ) {
 		this.logger = PraqmaLogger.getLogger( logger );
 		System.out.println( this.logger.toString() );
 	}
-	
-	private CommandLine()
-	{
+
+	private CommandLine() {
 		os = System.getProperty( "os.name" );
 		logger.debug( "Running on " + os );
-		if( os.matches( "^.*(?i)windows.*$" ) )
-		{
+		if( os.matches( "^.*(?i)windows.*$" ) ) {
 			logger.debug( "Using special windows environment" );
 			cmd = new String[3];
 			cmd[0] = "cmd.exe";
 			cmd[1] = "/C";
 			last = 2;
-		}
-		else
-		{
-		    	thisos = OperatingSystem.UNIX;
+		} else {
+			thisos = OperatingSystem.UNIX;
 			cmd = new String[1];
 		}
 	}
-	
-	public OperatingSystem getOS()
-	{
-	    return thisos;
+
+	public OperatingSystem getOS() {
+		return thisos;
 	}
-	
-	public static CommandLine getInstance()
-	{
+
+	public static CommandLine getInstance() {
 		return instance;
 	}
-	
-	public CmdResult run( String cmd ) throws CommandLineException, AbnormalProcessTerminationException
-	{
+
+	public CmdResult run( String cmd ) throws CommandLineException, AbnormalProcessTerminationException {
 		return run( cmd, null, true, false );
 	}
-	
-	public CmdResult run( String cmd, File dir ) throws CommandLineException, AbnormalProcessTerminationException
-	{
+
+	public CmdResult run( String cmd, File dir ) throws CommandLineException, AbnormalProcessTerminationException {
 		return run( cmd, dir, true, false );
 	}
-	
-	public CmdResult run( String cmd, File dir, boolean merge ) throws CommandLineException, AbnormalProcessTerminationException
-	{
+
+	public CmdResult run( String cmd, File dir, boolean merge ) throws CommandLineException, AbnormalProcessTerminationException {
 		return run( cmd, dir, merge, false );
 	}
-	
+
 	/**
 	 * Execute a command line operation.
-	 * @param cmd The command itself
-	 * @param dir The working directory
-	 * @param merge Merge stderror with stdout
-	 * @param ignore Ignore any abnormal process terminations. This will allow the output to be returned without exceptions to be thrown.
+	 * 
+	 * @param cmd
+	 *            The command itself
+	 * @param dir
+	 *            The working directory
+	 * @param merge
+	 *            Merge stderror with stdout
+	 * @param ignore
+	 *            Ignore any abnormal process terminations. This will allow the
+	 *            output to be returned without exceptions to be thrown.
 	 * @return
 	 * @throws CommandLineException
 	 * @throws AbnormalProcessTerminationException
 	 */
-	public CmdResult run( String cmd, File dir, boolean merge, boolean ignore ) throws CommandLineException, AbnormalProcessTerminationException
-	{
-		logger.trace_function();		
+	public CmdResult run( String cmd, File dir, boolean merge, boolean ignore ) throws CommandLineException, AbnormalProcessTerminationException {
+		logger.trace_function();
 		logger.debug( "$ " + cmd );
-		
+
 		/*
-		String[] cmds = new String[3];
-		cmds[0] = "cmd.exe";
-		cmds[1] = "/C";
-		cmds[2] = cmd;
-		*/
-		
-		//cmd = this.cmd + cmd;
+		 * String[] cmds = new String[3]; cmds[0] = "cmd.exe"; cmds[1] = "/C";
+		 * cmds[2] = cmd;
+		 */
+
+		// cmd = this.cmd + cmd;
 		this.cmd[last] = cmd;
-		
-		
-		try
-		{
+
+		try {
 			ProcessBuilder pb = new ProcessBuilder( this.cmd );
 			pb.redirectErrorStream( merge );
-			
-			if( dir != null )
-			{
+
+			if( dir != null ) {
 				logger.debug( "Executing command in " + dir );
 				pb.directory( dir );
 			}
-			
+
 			CmdResult result = new CmdResult();
 			Process p = pb.start();
 
@@ -123,75 +110,58 @@ public class CommandLine implements CommandLineInterface
 
 			output.start();
 			errors.start();
-			
+
 			int exitValue = 0;
-			try
-			{
+			try {
 				exitValue = p.waitFor();
-			}
-			catch( InterruptedException e )
-			{
+			} catch (InterruptedException e) {
 				p.destroy();
+			} finally {
+				Thread.interrupted();
 			}
-            finally
-            {
-                Thread.interrupted();
-            }
-            
-			try
-			{
+
+			try {
 				output.join();
-			}
-			catch ( InterruptedException e )
-			{
+			} catch (InterruptedException e) {
 				logger.error( "Could not join output thread" );
 			}
-			
-			try
-			{
+
+			try {
 				errors.join();
-			}
-			catch ( InterruptedException e )
-			{
+			} catch (InterruptedException e) {
 				logger.error( "Could not join errors thread" );
 			}
-			
+
 			/* Closing streams */
 			p.getErrorStream().close();
 			p.getInputStream().close();
-			
+
 			/* Abnormal process termination, with error out as message */
-			if ( exitValue != 0 )
-			{
+			if( exitValue != 0 ) {
 				logger.debug( "Abnormal process termination(" + exitValue + "): " + output.sres.toString() );
-				
-				/* Only throw the exception if it is not ignored, this is default */
-				if( !ignore )
-				{
-					if( merge )
-					{
+
+				/*
+				 * Only throw the exception if it is not ignored, this is
+				 * default
+				 */
+				if( !ignore ) {
+					if( merge ) {
 						throw new AbnormalProcessTerminationException( output.sres.toString() );
-					}
-					else
-					{
+					} else {
 						throw new AbnormalProcessTerminationException( errors.sres.toString() );
 					}
 				}
 			}
-			
 
-			
 			/* Setting command result */
 			result.stdoutBuffer = output.sres;
-			result.stdoutList   = output.lres;
-			
-			result.errorBuffer  = errors.sres;
-			result.errorList    = errors.lres;
-			
+			result.stdoutList = output.lres;
+
+			result.errorBuffer = errors.sres;
+			result.errorList = errors.lres;
+
 			return result;
-		}
-		catch ( IOException e )
-		{
+		} catch (IOException e) {
 			logger.warning( "Could not execute the command \"" + cmd + "\" correctly: " + e.getMessage() );
 			throw new CommandLineException( "Could not execute the command \"" + cmd + "\" correctly: " + e.getMessage() );
 		}
--- src/main/java/net/praqma/util/option/Options.java
+++ src/main/java/net/praqma/util/option/Options.java
@@ -21,208 +21,208 @@ import java.util.List;
  * 
  */
 public class Options {
-    private List<Option> options = new ArrayList<Option>();
-    private String syntax = "";
-    private String header = "";
-    private String description = "";
+	private List<Option> options = new ArrayList<Option>();
+	private String syntax = "";
+	private String header = "";
+	private String description = "";
 
-    private String version = "";
+	private String version = "";
 
-    private Option ohelp = null;
-    private Option oversion = null;
-    private Option overbose = null;
+	private Option ohelp = null;
+	private Option oversion = null;
+	private Option overbose = null;
 
-    private boolean verbose = false;
+	private boolean verbose = false;
 
-    public static final String linesep = System.getProperty("line.separator");
+	public static final String linesep = System.getProperty( "line.separator" );
 
-    /* CONSTRUCTORS */
+	/* CONSTRUCTORS */
 
-    public Options() {
+	public Options() {
 
-    }
+	}
 
-    public Options(String version) {
-	this.version = version;
-    }
+	public Options( String version ) {
+		this.version = version;
+	}
 
-    public void setDefaultOptions() {
-	ohelp = new Option("help", "h", false, 0, "Display help");
-	oversion = new Option("version", "v", false, 0, "Print the version");
-	overbose = new Option("verbose", "V", false, 0, "Verbose");
+	public void setDefaultOptions() {
+		ohelp = new Option( "help", "h", false, 0, "Display help" );
+		oversion = new Option( "version", "v", false, 0, "Print the version" );
+		overbose = new Option( "verbose", "V", false, 0, "Verbose" );
 
-	this.setOption(ohelp);
-	this.setOption(oversion);
-	this.setOption(overbose);
-    }
+		this.setOption( ohelp );
+		this.setOption( oversion );
+		this.setOption( overbose );
+	}
 
-    public void setSyntax(String syntax) {
-	this.syntax = syntax;
-    }
+	public void setSyntax( String syntax ) {
+		this.syntax = syntax;
+	}
 
-    public void setDescription(String desc) {
-	this.description = desc;
-    }
+	public void setDescription( String desc ) {
+		this.description = desc;
+	}
 
-    public void setHeader(String header) {
-	this.header = header;
-    }
+	public void setHeader( String header ) {
+		this.header = header;
+	}
 
-    public void setOption(Option option) {
-	options.add(option);
-    }
+	public void setOption( Option option ) {
+		options.add( option );
+	}
 
-    public void parse(String[] args) {
-	String currentStr = null;
-	Option current = null;
+	public void parse( String[] args ) {
+		String currentStr = null;
+		Option current = null;
 
-	for (int i = 0; i < args.length; i++) {
+		for( int i = 0; i < args.length; i++ ) {
 
-	    /* New option */
-	    if (args[i].startsWith("-")) {
+			/* New option */
+			if( args[i].startsWith( "-" ) ) {
 
-		/* Of the form --option */
-		if (args[i].startsWith("-", 1)) {
+				/* Of the form --option */
+				if( args[i].startsWith( "-", 1 ) ) {
 
-		    currentStr = args[i].substring(2);
-		    current = null;
-		    String[] val = currentStr.split("=", 2);
+					currentStr = args[i].substring( 2 );
+					current = null;
+					String[] val = currentStr.split( "=", 2 );
 
-		    if (val.length == 2) {
-			currentStr = val[0];
-		    }
+					if( val.length == 2 ) {
+						currentStr = val[0];
+					}
 
-		    for (Option o : options) {
-			if (currentStr.equalsIgnoreCase(o.longName)) {
-			    current = o;
-			    if (val.length == 2) {
-				o.addValue(val[1]);
-			    }
+					for( Option o : options ) {
+						if( currentStr.equalsIgnoreCase( o.longName ) ) {
+							current = o;
+							if( val.length == 2 ) {
+								o.addValue( val[1] );
+							}
 
-			    o.setUsed();
+							o.setUsed();
+						}
+					}
+				}
+				/* Single char option of the form -o */
+				else {
+					currentStr = args[i].substring( 1 );
+					current = null;
+					for( Option o : options ) {
+						if( currentStr.equals( o.shortName ) ) {
+							current = o;
+							o.setUsed();
+						}
+					}
+				}
+			} else {
+				if( current != null ) {
+					// options.get( current ).values.add( args[i] );
+					current.addValue( args[i] );
+				}
 			}
-		    }
 		}
-		/* Single char option of the form -o */
-		else {
-		    currentStr = args[i].substring(1);
-		    current = null;
-		    for (Option o : options) {
-			if (currentStr.equals(o.shortName)) {
-			    current = o;
-			    o.setUsed();
+	}
+
+	private void helpUsed() {
+		if( ohelp != null && ohelp.used ) {
+			this.display();
+			System.exit( 0 );
+		}
+	}
+
+	private void versionUsed() {
+		if( oversion != null && oversion.used ) {
+			System.out.println( this.version );
+			System.exit( 0 );
+		}
+	}
+
+	private void verboseUsed() {
+		if( overbose != null && overbose.used ) {
+			this.verbose = true;
+		}
+	}
+
+	public void checkOptions() throws Exception {
+		this.helpUsed();
+		this.versionUsed();
+		this.verboseUsed();
+
+		String errors = "";
+
+		for( Option o : options ) {
+			if( o.required && !o.used ) {
+				errors += o.longName + " is not used and is not optional." + linesep;
 			}
-		    }
-		}
-	    } else {
-		if (current != null) {
-		    // options.get( current ).values.add( args[i] );
-		    current.addValue(args[i]);
-		}
-	    }
-	}
-    }
 
-    private void helpUsed() {
-	if (ohelp != null && ohelp.used) {
-	    this.display();
-	    System.exit(0);
-	}
-    }
-
-    private void versionUsed() {
-	if (oversion != null && oversion.used) {
-	    System.out.println(this.version);
-	    System.exit(0);
-	}
-    }
-
-    private void verboseUsed() {
-	if (overbose != null && overbose.used) {
-	    this.verbose = true;
-	}
-    }
-
-    public void checkOptions() throws Exception {
-	this.helpUsed();
-	this.versionUsed();
-	this.verboseUsed();
-
-	String errors = "";
-
-	for (Option o : options) {
-	    if (o.required && !o.used) {
-		errors += o.longName + " is not used and is not optional." + linesep;
-	    }
-
-	    if (o.arguments != o.values.size() && o.arguments > -1 && o.used) {
-		errors += "Incorrect arguments for option " + o.longName + ". "	+ o.arguments + " required." + linesep;
-	    }
-	}
-
-	if (errors.length() > 0) {
-	    throw new Exception(errors);
-	}
-    }
-
-    /**
-     * @deprecated since 0.1.8
-     * @return
-     */
-    public boolean verbose() {
-	return this.verbose;
-    }
-    
-    public boolean isVerbose() {
-	return this.verbose;
-    }
-
-    public void print() {
-	System.out.println("Printing " + options.size() + " option" + (options.size() == 1 ? "" : "s"));
-
-	for (Option o : options) {
-	    System.out.print(o.longName + ": ");
-	    // System.out.print( ( o.required ? "Required " : "Optional " ) );
-	    System.out.print((o.used ? "Used " : "Unused "));
-	    if (o.values.size() > 0) {
-		System.out.print("Values:");
-		int c = 0;
-		for (String s : o.values) {
-		    c++;
-		    System.out.print(" [" + c + "] " + s);
+			if( o.arguments != o.values.size() && o.arguments > -1 && o.used ) {
+				errors += "Incorrect arguments for option " + o.longName + ". " + o.arguments + " required." + linesep;
+			}
 		}
 
-	    }
-	    System.out.println("");
-	}
-    }
-
-    public void display() {
-	if (this.header.length() > 0) {
-	    System.out.println(this.header + linesep);
+		if( errors.length() > 0 ) {
+			throw new Exception( errors );
+		}
 	}
 
-	System.out.println("Usage: " + this.syntax + linesep);
-
-	for (Option o : options) {
-	    System.out.print("  --" + o.longName);
-
-	    if (o.shortName.length() > 0) {
-		System.out.print(new String(new char[15 - o.longName.length()]).replace("\0", " ") + "-" + o.shortName);
-	    }
-
-	    System.out.print("\t" + (o.required ? "Required" : "Optional"));
-
-	    System.out.println("\t" + o.description);
+	/**
+	 * @deprecated since 0.1.8
+	 * @return
+	 */
+	public boolean verbose() {
+		return this.verbose;
 	}
 
-	if (this.description.length() > 0) {
-	    System.out.println(linesep + this.description);
+	public boolean isVerbose() {
+		return this.verbose;
 	}
-    }
 
-    public String toString() {
-	return "";
-    }
+	public void print() {
+		System.out.println( "Printing " + options.size() + " option" + ( options.size() == 1 ? "" : "s" ) );
+
+		for( Option o : options ) {
+			System.out.print( o.longName + ": " );
+			// System.out.print( ( o.required ? "Required " : "Optional " ) );
+			System.out.print( ( o.used ? "Used " : "Unused " ) );
+			if( o.values.size() > 0 ) {
+				System.out.print( "Values:" );
+				int c = 0;
+				for( String s : o.values ) {
+					c++;
+					System.out.print( " [" + c + "] " + s );
+				}
+
+			}
+			System.out.println( "" );
+		}
+	}
+
+	public void display() {
+		if( this.header.length() > 0 ) {
+			System.out.println( this.header + linesep );
+		}
+
+		System.out.println( "Usage: " + this.syntax + linesep );
+
+		for( Option o : options ) {
+			System.out.print( "  --" + o.longName );
+
+			if( o.shortName.length() > 0 ) {
+				System.out.print( new String( new char[15 - o.longName.length()] ).replace( "\0", " " ) + "-" + o.shortName );
+			}
+
+			System.out.print( "\t" + ( o.required ? "Required" : "Optional" ) );
+
+			System.out.println( "\t" + o.description );
+		}
+
+		if( this.description.length() > 0 ) {
+			System.out.println( linesep + this.description );
+		}
+	}
+
+	public String toString() {
+		return "";
+	}
 
 }
