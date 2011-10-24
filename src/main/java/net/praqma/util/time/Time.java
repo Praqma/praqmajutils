31f62abaf75984dbbfb9c0bd34cdde747e967cb2
wolfgarnet <wolfgarnet@gmail.com>
2011-10-24 10:38:34 +0200

    Fixed tiem

--- src/main/java/net/praqma/util/time/Time.java
+++ src/main/java/net/praqma/util/time/Time.java
@@ -1,25 +1,23 @@
 package net.praqma.util.time;
 
 public class Time {
+	
 	public static String longToTime( long time ) {
-		
 		int hours = 0;
 		int minutes = 0;
 		int seconds = 0;
 		
 		time /= 1000;
 		
-		String t = "";
-		
 		/* Hour */
 		if( time > 3600 ) {
-			hours = (int) ( time % 3600 );
-			time -= ( hours * 3600 );
+			hours = (int) ( time / 3600 );
+			time = time % 3600;
 		}
 		
 		if( time > 60 ) {
-			minutes = (int) ( time % 60 );
-			time -= ( minutes * 60 );
+			minutes = (int) ( time / 60 );
+			time = time % 60;
 		}
 		
 		seconds = (int) time;
