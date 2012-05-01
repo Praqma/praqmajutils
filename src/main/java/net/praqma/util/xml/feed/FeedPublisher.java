9d8a42748a185316541a7312d555a380923a43ef
wolfgarnet <wolfgarnet@gmail.com>
2012-05-01 08:50:26 +0200

    Added time zones to feeds

--- src/main/java/net/praqma/util/xml/feed/FeedPublisher.java
+++ src/main/java/net/praqma/util/xml/feed/FeedPublisher.java
@@ -6,7 +6,7 @@ import java.util.Date;
 import net.praqma.util.xml.XML;
 
 public abstract class FeedPublisher extends XML {
-	public static SimpleDateFormat format = new SimpleDateFormat( "yyyy-MM-dd'T'HH:mm:ss'Z'" );
+	public static SimpleDateFormat format = new SimpleDateFormat( "yyyy-MM-dd'T'HH:mm:ssZ" );
 	
 	public abstract String toFeed( Feed feed, int limit ) throws FeedException;
 	public abstract Feed fromFeed( XML xml ) throws FeedException;
