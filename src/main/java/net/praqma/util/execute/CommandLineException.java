cacae3107a75bd118ad6769de53379171c50bd1a
wolfgarnet <wolfgarnet@gmail.com>
2012-07-05 08:56:30 +0200

    Refactored command line exception

--- src/main/java/net/praqma/util/execute/CommandLineException.java
+++ src/main/java/net/praqma/util/execute/CommandLineException.java
@@ -1,20 +1,9 @@
 package net.praqma.util.execute;
 
-public class CommandLineException extends RuntimeException
-{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -8031565757415076147L;
-//TODO remove this is not used any where in the code..
-//	CommandLineException()
-//	{
-//		super(); 
-//	}
-	
-	CommandLineException( String s )
-	{
-		super( s ); 
+public class CommandLineException extends RuntimeException {
+
+	CommandLineException( String s ) {
+		super( s );
 	}
 
 }
