5a6a11fb76251ff05d4f75ad9d9851ba5e3d63d0
Christian Wolfgang <wolfgarnet@gmail.com>
2012-11-09 12:20:26 +0100

    Updated formatter

--- src/main/java/net/praqma/logging/PraqmaticLogFormatter.java
+++ src/main/java/net/praqma/logging/PraqmaticLogFormatter.java
@@ -1,57 +1,73 @@
 package net.praqma.logging;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.text.MessageFormat;
 import java.util.Date;
 import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 
 public class PraqmaticLogFormatter extends Formatter {
-	
-	public String format = "";
-	
-	private static final int width = 8;
-	private boolean enableLineNumbers = true;
-	
-	private MessageFormat messageFormat = new MessageFormat( "{3,date,hh:mm:ss} [{1}]{5} {6}.{7}, {99}: {4} \n" );
-	
-	public PraqmaticLogFormatter() {}
-	
-	public PraqmaticLogFormatter( String format ) {
-		messageFormat = new MessageFormat( format );
-		this.format = format;
-	}
 
-	@Override
-	public String format( LogRecord record ) {
-		
-		Object[] args = new Object[100];
-		args[0] = record.getLoggerName();
-		args[1] = record.getLevel();
-		args[2] = Thread.currentThread().getName();
-		args[3] = new Date( record.getMillis() );
-		args[4] = record.getMessage();
-		int w = width - record.getLevel().getName().length();
-		if( w > 0 ) {
-			args[5] = new String( new char[w] ).replace( "\0", " " );
-		} else {
-			args[5] = "";
-		}
-		
-		args[6] = record.getSourceClassName();
-		args[7] = record.getSourceMethodName();
-		
-		
-		if( enableLineNumbers ) {
-			try {
-				args[99] = Thread.currentThread().getStackTrace()[8].getLineNumber();
-			} catch( Exception e ) {
-				args[99] = -1;
-			}
-		} else {
-			args[99] = "?";
-		}
-		
-		return messageFormat.format( args );
-	}
+    public String format = "";
+
+    private static final int width = 8;
+    private boolean enableLineNumbers = true;
+
+    private MessageFormat messageFormat = new MessageFormat( "{3,date,HH:mm:ss} [{1}]{5} {6}.{7}, {19}: {4} \n{8}" );
+
+    public PraqmaticLogFormatter() {}
+
+    public PraqmaticLogFormatter( String format ) {
+        messageFormat = new MessageFormat( format );
+        this.format = format;
+    }
+
+    @Override
+    public String format( LogRecord record ) {
+
+        Object[] args = new Object[20];
+        args[0] = record.getLoggerName();
+        args[1] = record.getLevel();
+        args[2] = Thread.currentThread().getName();
+        args[3] = new Date( record.getMillis() );
+        args[4] = record.getMessage();
+
+        int w = width - record.getLevel().getName().length();
+        if( w > 0 ) {
+            args[5] = new String( new char[w] ).replace( "\0", " " );
+        } else {
+            args[5] = "";
+        }
+
+        args[6] = record.getSourceClassName();
+        args[7] = record.getSourceMethodName();
+        if( record.getThrown() != null ) {
+            try {
+                StringWriter sw = new StringWriter();
+                PrintWriter pw = new PrintWriter(sw);
+                record.getThrown().printStackTrace(pw);
+                pw.close();
+                args[8] = sw.toString();
+            } catch (Exception ex) {
+                args[8] = "?";
+            }
+        } else {
+            args[8] = "";
+        }
+
+
+        if( enableLineNumbers ) {
+            try {
+                args[19] = Thread.currentThread().getStackTrace()[8].getLineNumber();
+            } catch( Exception e ) {
+                args[19] = -1;
+            }
+        } else {
+            args[19] = "?";
+        }
+
+        return messageFormat.format( args );
+    }
 
 }
